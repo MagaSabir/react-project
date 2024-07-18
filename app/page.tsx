28ad02674b3db39bc7aadd3c0b35b7076926ec6e
MSabir <magsabir95@gmail.com>
2024-07-18 18:25:43 +0300

    Init commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <button className="text-black font-bold hover:text-red-600 bg-yellow-400 w-32 h-8 rounded-lg">Hello World</button>
+    </main>
+  );
+}
