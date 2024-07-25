052d0d4e5f4698cf511e8491de025e454c49e7fe
MSabir <magsabir95@gmail.com>
2024-07-25 23:21:52 +0300

    File update

--- my-app/app/auth/page.tsx
+++ my-app/app/auth/page.tsx
@@ -1,17 +1,51 @@
 'use client'
 import Input from "@/components/input";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import { useCallback, useState } from "react";
 
 const Auth = () => {
 	const [email, setEmail] = useState('')
 	const [name, setName] = useState('')
 	const [password, setPassword] = useState('')
+	const router = useRouter()
 
 	const [variant, setVariant] = useState('login');
 
 	const toggleVariant = useCallback(() => {
 		setVariant((currentVariant) => currentVariant === 'login' ? 'register': 'login')
 	},[])
+
+	const login = useCallback(async () => {
+		try {
+			await signIn('credentials', {
+				email,
+				password,
+				redirect: false,
+				callbackUrl: '/'
+			});
+
+			router.push('/')
+		} catch (error) {
+			console.log(error) 
+		}
+	}, [email, password, router])
+
+	const register = useCallback(async () => {
+		try {
+			await axios.post('/api/register', {
+				email,
+				name,
+				password
+			});
+			login()
+		} catch (error) {
+			console.log(error)
+		}
+	}, [email,name,password, login])
+
 
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero2.jpg')] bg-no-repeat bg-fixed bg-cover">
@@ -48,7 +82,7 @@ const Auth = () => {
 							value={password}
 						/>
 					</div>
-					<button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+					<button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
 						{variant === 'login' ? 'Login' : 'Sign up'}
 					</button>
 					<p className="text-neutral-500 mt-12">
